8816a3e586dfcbe4bc09cd765ba0f5b9fd9d1837
Dmitriy Borovikov <dmitry@borovikov.name>
2019-02-11 17:04:36 +0300

    Assets & Animations

---
+++ NaturalistApp/Animations.swift
@@ -0,0 +1,30 @@
+//
+//  Animations.swift
+//  githubApp
+//
+//  Created by Dmitriy Borovikov on 19/12/2018.
+//  Copyright © 2018 Dmitriy Borovikov. All rights reserved.
+//
+
+import UIKit
+
+class Animations {
+    @discardableResult
+    static func jiggle(view: UIView) -> UIViewPropertyAnimator {
+        return UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.33, delay: 0, animations: {
+            UIView.animateKeyframes(withDuration: 1, delay: 0, animations: {
+                UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25) {
+                    view.transform = CGAffineTransform(rotationAngle: -.pi/32)
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.75) {
+                    view.transform = CGAffineTransform(rotationAngle: +.pi/32)
+                }
+                UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 1.0) {
+                    view.transform = CGAffineTransform.identity
+                }
+            }, completion: nil)
+        }, completion: { _ in
+            view.transform = .identity
+        })
+    }
+}
