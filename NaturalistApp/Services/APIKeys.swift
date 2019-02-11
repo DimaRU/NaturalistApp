38e1000b616b611089f56143d3f29233e9d24d35
Dmitriy Borovikov <dmitry@borovikov.name>
2019-02-11 14:43:57 +0300

    KeychainService, APIKeys

---
+++ NaturalistApp/Services/APIKeys.swift
@@ -0,0 +1,22 @@
+////
+///  APIKeys.swift
+//
+
+import Keys
+
+
+// Mark: - API Keys
+struct APIKeys {
+    let clientId: String
+    let clientSecret: String
+
+    // MARK: Shared Keys
+    static let `default`: APIKeys = {
+        return APIKeys(
+            clientId: NaturalistAppKeys().clientId,
+            clientSecret: NaturalistAppKeys().clientSecret
+            )
+    }()
+    
+    static var shared = APIKeys.default
+}
---
+++ NaturalistApp/Services/KeychainService.swift
@@ -0,0 +1,37 @@
+//
+//  KeychainService.swift
+//
+//  Created by Dmitriy Borovikov on 08.07.17.
+//  Copyright © 2017 Dmitriy Borovikov. All rights reserved.
+//
+
+import UIKit
+import KeychainAccess
+
+class KeychainService {
+    public enum KeychainKeys: String {
+        case login
+        case password
+        case accessToken
+    }
+
+    let keychain: Keychain
+    static let shared = KeychainService()
+    
+    init() {
+        // Init keychain access
+        let bundleId = Bundle.main.bundleIdentifier!
+        keychain = Keychain(service: bundleId)
+    }
+    
+    subscript(key: KeychainKeys) -> String? {
+        get {
+            return keychain[key.rawValue]
+        }
+        
+        set {
+            keychain[key.rawValue] = newValue
+        }
+    }
+    
+}
