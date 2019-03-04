67cf19bbab1e81d0c56197c4c97939420c0af324
Dmitriy Borovikov <dmitry@borovikov.name>
2019-03-04 22:44:42 +0300

    UIViewExtensions

---
+++ NaturalistApp/Extensions/UIViewExtensions.swift
@@ -0,0 +1,39 @@
+//
+//  UIViewExtensions.swift
+//  TestInstantiate
+//
+//  Created by Dmitriy Borovikov on 04/03/2019.
+//  Copyright © 2019 Dmitriy Borovikov. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+
+}
