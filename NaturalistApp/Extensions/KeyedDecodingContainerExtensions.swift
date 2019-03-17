28ea3c2ac6302f1eaba816de0a8c00a8ecce26c1
Dmitriy Borovikov <dmitry@borovikov.name>
2019-03-17 15:57:27 +0300

    KeyedDecodingContainer refactoring

--- NaturalistApp/Extensions/KeyedDecodingContainerExtensions.swift
+++ NaturalistApp/Extensions/KeyedDecodingContainerExtensions.swift
@@ -11,21 +11,17 @@ import Foundation
 public extension KeyedDecodingContainer  {
     public func decode(_ type: URL.Type, forKey key: Key) throws -> URL {
         let stringValue = try self.decode(String.self, forKey: key)
-        guard let urlStringSafe = stringValue.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) else {
-            let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Invalid URL string - 1.")
-            throw DecodingError.dataCorrupted(context)
-        }
-        guard let url = URL(string: urlStringSafe) else {
-            let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Invalid URL string - 2.")
-            throw DecodingError.dataCorrupted(context)
-        }
-        return url
+        return try decodeURL(from: stringValue)
     }
-
+    
     func decodeIfPresent(_ type: URL.Type, forKey key: Key) throws -> URL? {
         guard let stringValue = try self.decodeIfPresent(String.self, forKey: key) else {
             return nil
         }
+        return try decodeURL(from: stringValue)
+    }
+    
+    fileprivate func decodeURL(from stringValue: String) throws -> URL {
         guard let urlStringSafe = stringValue.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) else {
             let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Invalid URL string - 1.")
             throw DecodingError.dataCorrupted(context)
