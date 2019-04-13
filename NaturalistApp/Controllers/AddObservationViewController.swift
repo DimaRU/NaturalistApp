440e9c7e9add8eb97d8d5fcfea09b58a30fe0718
Dmitriy Borovikov <dmitry@borovikov.name>
2019-04-13 19:53:35 +0300

    AddObservationViewController template

---
+++ NaturalistApp/Controllers/AddObservationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddObservationViewController.swift
+//  NaturalistApp
+//
+//  Created by Dmitriy Borovikov on 13/04/2019.
+//  Copyright © 2019 Dmitriy Borovikov. All rights reserved.
+//
+
+import UIKit
+
+class AddObservationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
