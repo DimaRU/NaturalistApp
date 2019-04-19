16bb633faafe88d383944b84d46965c0b675e63c
Dmitriy Borovikov <dmitry@borovikov.name>
2019-04-20 00:06:05 +0300

    EditLocationViewController,  TaxaSearchViewController template

---
+++ NaturalistApp/Controllers/TaxaSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaxaSearchViewController.swift
+//  NaturalistApp
+//
+//  Created by Dmitriy Borovikov on 19/04/2019.
+//  Copyright © 2019 Dmitriy Borovikov. All rights reserved.
+//
+
+import UIKit
+
+class TaxaSearchViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
