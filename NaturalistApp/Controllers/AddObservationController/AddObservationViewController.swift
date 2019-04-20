1d0f4cea4495bf19001e11704e970b9dcaafa624
Dmitriy Borovikov <dmitry@borovikov.name>
2019-04-20 16:16:49 +0300

    Prepare for computervision test

--- NaturalistApp/Controllers/AddObservationController/AddObservationViewController.swift
+++ NaturalistApp/Controllers/AddObservationController/AddObservationViewController.swift
@@ -27,11 +27,28 @@ class AddObservationViewController: UIViewController, MainStoryboardInstantiable
         pickPhotoController = (children.first as! PickPhotoController)
         observationTableController = (children.last as! AddObservationTableViewController)
         pickPhotoController.delegate = self
+        authSession()
     }
 
     @IBAction func shareButtonTap(_ sender: Any) {
         print("Share")
     }
+
+    private func authSession() {
+        guard let bearer = KeychainService.shared[.bearer] else { return }
+        NatProvider.shared.request(.apiToken(bearer: bearer))
+            .then { (jwt: JWTToken) -> Promise<PagedResults<User>> in
+                KeychainService.shared[.apiToken] = jwt.apiToken
+                return NatProvider.shared.request(.currentUser)
+            }.done { (paged: PagedResults<User>) -> Void in
+                let user = paged.results.content.first!
+                Globals.setCurrentUser(user: user)
+                print(user.login, user.name ?? "")
+            }.catch { error in
+                print(error)
+        }
+    }
+
 }
 
 extension AddObservationViewController: PickPhotoControllerProtocol {
